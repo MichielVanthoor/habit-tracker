5e18767a821241d17240cbe3547b584cb21739de
Michiel Vanthoor <michielvanthoor@gmail.com>
2020-01-12 14:20:36 +0100

    Adding SQLAlchemy

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 
 import os
+import sqlalchemy as db
 
 from flask import render_template, request
 from flask_sslify import SSLify
@@ -9,6 +10,11 @@ from werkzeug.contrib.fixers import ProxyFix
 # Configure HTTPS redirect
 #app.wsgi_app = ProxyFix(app.wsgi_app)
 #SSLify(app)
+
+# Configure SQLAlchemy connection
+engine = db.create_engine('mysql+mysqldb://miva:fbbjDPcv8wwsrhwq@35.233.54.56/habit_tracker')
+connection = engine.connect()
+habits = db.Table('habits', metadata, autoload=True, autoload_with=engine)
 
 # Overview of all the routes
 @app.route('/')
--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Starting the engines!
-exec gunicorn -b 0.0.0.0:$PORT --access-logfile - --error-logfile - app:app
+exec gunicorn -b 0.0.0.0:$PORT --reload --access-logfile - --error-logfile - app:app
