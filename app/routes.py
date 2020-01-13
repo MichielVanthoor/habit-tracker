21d9b34bc791ffd99f1669c23acf84fdb3b3c509
Michiel Vanthoor <michielvanthoor@gmail.com>
2020-01-13 19:25:26 +0100

    Make code PEP8 compliant

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,20 @@
 from app import app
 from app import utils
 
-import os
+
 import sqlalchemy as db
 
-from flask import render_template, request
+
+from flask import render_template
+
 
 # Configure SQLAlchemy connection
-engine = db.create_engine('mysql+mysqldb://miva:fbbjDPcv8wwsrhwq@35.233.54.56/habit_tracker')
+engine = db.create_engine(
+    'mysql+mysqldb://miva:fbbjDPcv8wwsrhwq@35.233.54.56/habit_tracker')
 connection = engine.connect()
 metadata = db.MetaData()
 habits = db.Table('habits', metadata, autoload=True, autoload_with=engine)
+
 
 # Overview of all the routes
 @app.route('/')
@@ -23,4 +27,4 @@ def index():
     output = utils.generate_output(ResultSet)
     num = len(output)
 
-    return render_template('index.html', num_dates = num, results = output)
+    return render_template('index.html', num_dates=num, results=output)
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+
 def create_html(row):
     html_list = []
 
@@ -7,65 +8,65 @@ def create_html(row):
     html_list.append(row[0].strftime("%a, %b %d"))
 
     # Add html if weekend day
-    if row[0].weekday() >=5:
-      html_list.append('info')
+    if row[0].weekday() >= 5:
+        html_list.append('info')
     else:
-      html_list.append('')
+        html_list.append('')
 
     if len(row) > 2:
-      # Detect 'snoozed'
-      if row[1] == 0:
-        html_list.append('warning')
-      else:
-        html_list.append('danger')
+        # Detect 'snoozed'
+        if row[1] == 0:
+            html_list.append('warning')
+        else:
+            html_list.append('danger')
 
-      # Detect 'water_drank'
-      if row[6] == 1:
-        html_list.append('warning')
-      else:
-        html_list.append('danger')
+        # Detect 'water_drank'
+        if row[6] == 1:
+            html_list.append('warning')
+        else:
+            html_list.append('danger')
 
-      # Detect 'teeth_brushed_am'
-      if row[2] == 1:
-        html_list.append('warning')
-      else:
-        html_list.append('danger')
+        # Detect 'teeth_brushed_am'
+        if row[2] == 1:
+            html_list.append('warning')
+        else:
+            html_list.append('danger')
 
-      # Detect 'www_used_am'
-      if row[4] == 0:
-        html_list.append('warning')
-      else:
-        html_list.append('danger')
+        # Detect 'www_used_am'
+        if row[4] == 0:
+            html_list.append('warning')
+        else:
+            html_list.append('danger')
 
-      # Detect 'teeth_brushed_pm'
-      if row[3] == 1:
-        html_list.append('warning')
-      else:
-        html_list.append('danger')
+        # Detect 'teeth_brushed_pm'
+        if row[3] == 1:
+            html_list.append('warning')
+        else:
+            html_list.append('danger')
 
-      # Detect 'www_used_pm'
-      if row[5] == 0:
-        html_list.append('warning')
-      else:
-        html_list.append('danger')
+        # Detect 'www_used_pm'
+        if row[5] == 0:
+            html_list.append('warning')
+        else:
+            html_list.append('danger')
 
     return html_list
+
 
 def generate_output(resultset):
     today = datetime.today().date()
 
     output = []
-    for i in range(0,6):
-      row = []
-      day_it = today - timedelta(days= i)
-      row.append(day_it)
+    for i in range(0, 6):
+        row = []
+        day_it = today - timedelta(days=i)
+        row.append(day_it)
 
-      for day in resultset:
-        if day_it == day[1]:
-          row.extend(list(day[2:]))
+        for day in resultset:
+            if day_it == day[1]:
+                row.extend(list(day[2:]))
+        row = create_html(row)
 
-      row = create_html(row)
-
-      output.append(row)
+        output.append(row)
 
     return output
