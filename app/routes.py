13f455ce776607e4c84cd967b1ef5e523e2c5eb5
Michiel Vanthoor <michielvanthoor@gmail.com>
2020-01-22 21:46:31 +0100

    Adding FireStore in JS

--- app/routes.py
+++ app/routes.py
@@ -2,29 +2,30 @@ from app import app
 from app import utils
 
 
-import sqlalchemy as db
+#import sqlalchemy as db
 
 
 from flask import render_template
 
 
 # Configure SQLAlchemy connection
-engine = db.create_engine(
-    'mysql+mysqldb://miva:fbbjDPcv8wwsrhwq@35.233.54.56/habit_tracker')
-connection = engine.connect()
-metadata = db.MetaData()
-habits = db.Table('habits', metadata, autoload=True, autoload_with=engine)
+#engine = db.create_engine(
+    #'mysql+mysqldb://miva:fbbjDPcv8wwsrhwq@35.233.54.56/habit_tracker')
+#connection = engine.connect()
+#metadata = db.MetaData()
+#habits = db.Table('habits', metadata, autoload=True, autoload_with=engine)
 
 
 # Overview of all the routes
 @app.route('/')
 @app.route('/index')
 def index():
-    query = db.select([habits]).where(habits.columns.user_id == 'miva')
-    ResultProxy = connection.execute(query)
-    ResultSet = ResultProxy.fetchall()
+    #query = db.select([habits]).where(habits.columns.user_id == 'miva')
+    #ResultProxy = connection.execute(query)
+    #ResultSet = ResultProxy.fetchall()
 
-    results = utils.generate_output(ResultSet)
+    results = []
+    #results = utils.generate_output(ResultSet)
     num_dates = len(results)
 
     streaks = [2,2,2,2,2,2]
